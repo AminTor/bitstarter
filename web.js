56fe39d38f9e1e6705c01cee3b18cf4a4e87cc3f
Amin Torabkhani <amin.torabkhani@gmail.com>
2013-07-08 02:52:38 +0000

    testing why web.js is failing

--- web.js
+++ web.js
@@ -11,7 +11,8 @@ var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'));
 
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString("UTF-8"));
+    response.send("Hellow world test");
+//    response.send(fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString("UTF-8"));
 });
 
 var port = process.env.PORT || 5000;
