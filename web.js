625626732ceebebe02b715d401096882f4bc1602
Amin Torabkhani <amin.torabkhani@gmail.com>
2013-07-08 03:02:34 +0000

    cleaned web.js

--- web.js
+++ web.js
@@ -6,13 +6,13 @@ var fs = require('fs');
 var filename = '/home/ubuntu/bitstarter/index.html';
 
 
-var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'));
+//var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'));
+//    response.send(fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString("UTF-8"));
 
 
 
 app.get('/', function(request, response) {
-    response.send("Hellow world test");
-//    response.send(fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString("UTF-8"));
+    response.send('Hellow world test');
 });
 
 var port = process.env.PORT || 5000;
