1bcb3e1776f6a9ddb0e0d902cf9ff2cc5c30d706
Amin Torabkhani <amin.torabkhani@gmail.com>
2013-07-08 03:12:33 +0000

    removed home diretory from index.html

--- web.js
+++ web.js
@@ -7,12 +7,12 @@ var filename = '/home/ubuntu/bitstarter/index.html';
 
 
 //var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'));
-//    response.send(fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString("UTF-8"));
+//fs.readFileSync('index.html').toString("UTF-8");
 
 
 
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString("UTF-8"));
+    response.send(fs.readFileSync('index.html').toString("UTF-8"));
 });
 
 var port = process.env.PORT || 5000;
