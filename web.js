e214d8be267795920287f13b58a3a20497a3b1e4
Amin Torabkhani <amin.torabkhani@gmail.com>
2013-07-08 02:33:23 +0000

    updated web.js to read index.html

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var filename = '/home/ubuntu/bitstarter/index.html';
+
+
+var buf = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html'));
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString("utf-8",0,buf.length));
 });
 
 var port = process.env.PORT || 5000;
