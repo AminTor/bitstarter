96310c420fc41446f1abe64517f0cbac1f071b6e
Amin Torabkhani <king@amin-ubuntu.(none)>
2013-08-18 22:40:06 -0400

    added hello world to web.js to test

--- web.js
+++ web.js
@@ -12,10 +12,11 @@ var filename = '/home/ubuntu/bitstarter/index.html';
 
 
 app.get('/', function(request, response) {
+    response.send('Hello world22222211!');
     response.send(fs.readFileSync('index.html').toString("UTF-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8888;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
